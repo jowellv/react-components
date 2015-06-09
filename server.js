c77c4d9274d8febc583b6b31756cdca6e44efa6b
Jowell Villasin <jowell.villasin@gmail.com>
2015-06-08 20:16:17 -0700

    basic server

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var express = require('express');
+var app = express();
+
+var notesRoutes = express.Router();
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/notes_development');
+
+app.use(express.static(__dirname + '/build'));
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('server running on port ' + (process.env.PORT || 3000));
+});
